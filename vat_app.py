9d853e9366608faabf8ad1bed0703fe0489504dd
415-5-thanyapron <54255@yupparaj.ac.th>
2026-09-22 15:59:36 +0700

    vat_app.py

--- vat_app.py
+++ vat_app.py
@@ -2,10 +2,9 @@
 
 st.title("🛒แอปพลิเคชั่นคำนวณราคาสินค้ารวม VAT 7%")
 = st.numprice ber_input("กรอกราคาสินค้า (บาท):", value=0.0)
-vat = price * 0.07
-net_price = price - vat
-
-st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%): **{vat:.2f}** บาท")
+vat=price * 0.07
+net_price=price-vat
+st.header(f"• ภาษีมูลค่าเพิ่ม (VAT 7%):**{vat:.2f}** บาท")
 st.header(f"• ราคาสุทธิ: {net_price:.2f} บาท")
 st.divider()
 st.write("นางสาวธันยพร เชื้อหล้า เลขที่5 ม.4/15")
